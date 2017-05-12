7c71645bb616d1d3e2c8b6ec09d212c245ff8005
Orphée Faucoz <orphee.faucoz@gmail.com>
2017-05-12 14:26:48 +0200

    new

--- TP2/script/iris.R
+++ TP2/script/iris.R
@@ -9,8 +9,7 @@ irisquant <- iris[1:4]
 #summary(iris)
 visualisation <- function(){
   pca_iris <- prcomp(irisquant)
-  autoplot(pca_iris, loadings = TRUE, data=iris, colour='Species', loadings.colour = 'blue',
-           loadings.label = TRUE, loadings.label.size = 3)
+  autoplot(pca_iris, data=iris)
 }
 
 hclust_iris_dist <- function(){
