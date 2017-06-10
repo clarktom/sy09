5c43edca0b57bef82debf94c6bb4d290eaa20ab6
Orphée Faucoz <orphee.faucoz@gmail.com>
2017-06-10 17:48:07 +0200

    first spam version

--- TP4/script/fonctions/spam.R
+++ TP4/script/fonctions/spam.R
@@ -1,10 +1,3 @@
-Synth1_1000 <- read.csv("TP4/dataset/donnees/Synth1-1000.csv")
-Synth1_1000$title <- "synth1_1000"
-Synth2_1000 <- read.csv("TP4/dataset/donnees/Synth2-1000.csv")
-Synth2_1000$title <- "synth2_1000"
-Synth3_1000 <- read.csv("TP4/dataset/donnees/Synth3-1000.csv")
-Synth3_1000$title <- "synth3_1000"
-
 source("TP4/script/fonctions/anadisc.R")
 source("TP4/script/fonctions/separ1.R")
 source("TP4/script/fonctions/prob.ad.R")
@@ -13,8 +6,10 @@ source("TP4/script/fonctions/prob.log2.R")
 source("TP4/script/fonctions/log.R")
 source("TP4/script/fonctions/erreur.R")
 
-X <- Synth1_1000[,1:2]
-z <- Synth1_1000[,3]
+Donn <- read.csv("TP4/dataset/donnees/spam.csv", header=T)
+
+X <- Donn[,1:58]
+z <- Donn[,59]
   
 data_synth <- separ1(X,z)
 
@@ -25,12 +20,7 @@ log <- log.app(data_synth$Xapp,data_synth$zapp,1,1e-5)
 Xappq <- log.quadra(data_synth$Xapp)
 Xtstq <- log.quadra(data_synth$Xtst)
 logq <- log.app(Xappq, data_synth$zapp,1,1e-5)
-# val <- ad.val(adl,data_synth$Xtst)
-# err_adl <- erreur(20,X,z,"adl")
-# err_adq <- erreur(20,X,z,"adq")
-# err_nba <- erreur(20,X,z,"nba")
-# err_log <- erreur(20,X,z,"log")
-# err_logq <- erreur(20,X,z,"logq")
-# prob.ad(adl,data_synth$Xtst, data_synth$ztst, 0.5)
-# prob.log(log$beta, data_synth$Xtst, data_synth$ztst, 0.5)
+
+
+# prob.ad(nba,data_synth$Xtst, data_synth$ztst, 0.5)
 # prob.log2(logq$beta, data_synth$Xtst, data_synth$ztst, 0.5)
